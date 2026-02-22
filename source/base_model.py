76e1dedb9db87de6e237a6e1b52ae78b50147a8f
discpic-ron <granddriod1210@gmail.com>
2026-02-22 17:01:59 -0500

    Create base_model.py

---
+++ source/base_model.py
@@ -0,0 +1,22 @@
+class BaseModel:
+  def __init__(self,memory):
+    self.name = name
+    self.model = model
+    self.memory = memory
+    self.intents = {
+      "research": ["what is", "who is", "explain", "how", "why","when"],
+      "greeting": ["hello", "hi", "hey"],
+      "system": ["who are you"]
+    }
+    
+  def detect_intents(self, user_input):
+        text = user_input.lower()
+        found = []
+
+        for intent, keywords in self.intents.items():
+            for kw in keywords:
+                if kw in text:
+                    found.append(intent)
+                    break
+
+        return found if found else ["unknown"]
