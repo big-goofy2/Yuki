d32ee9d239e5540dee43b905c321da104e678889
discpic-ron <granddriod1210@gmail.com>
2026-03-30 18:10:03 -0400

    Delete source/tools/corrector.py

--- source/tools/corrector.py
+++
@@ -1,9 +0,0 @@
-import difflib
-
-def correct_word(word, dictionary):
-    matches = difflib.get_close_matches(word.lower(), dictionary, n=1, cutoff=0.75)
-
-    if matches:
-        return matches[0] # Returns the single best match
-    else:
-        return word # No close match found, return original word
