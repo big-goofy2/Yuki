a8581000e6dee983d6861c5ca6141d3a0d1cb722
discpic-ron <granddriod1210@gmail.com>
2026-04-29 16:29:56 -0400

    Refactor BaseModel initialization and remove intents
    Refactored BaseModel to initialize without parameters and integrated Memory class.

--- source/base_model.py
+++ source/base_model.py
@@ -1,21 +1,6 @@
+from memory import Memory
+
 class BaseModel:
-  def __init__(self,memory):
+  def __init__(self):
     self.model = "Numa-1"
-    self.memory = memory
-    self.intents = {
-      "research": ["what is", "who is", "explain", "how", "why","when"],
-      "greeting": ["hello", "hi", "hey"],
-      "system": ["who are you"]
-    }
-    
-  def detect_intents(self, user_input):
-        text = user_input.lower()
-        found = []
-
-        for intent, keywords in self.intents.items():
-            for kw in keywords:
-                if kw in text:
-                    found.append(intent)
-                    break
-
-        return found if found else ["unknown"]
+    self.memory = Memory()
