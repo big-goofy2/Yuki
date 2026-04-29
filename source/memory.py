f89e7263af82f93b9918c08612e423d3053de3c5
discpic-ron <granddriod1210@gmail.com>
2026-04-29 16:37:00 -0400

    Implement save method to write memory to file

--- source/memory.py
+++ source/memory.py
@@ -7,7 +7,8 @@ class Memory:
     self.long_term = {}
     
   def save(self):
-   pass
+   with open(self.filename, "w") as f:
+    json.dump({"short_term": self.short_term, "long_term": self.long_term}, f, indent=2)
    
   def load(self):
    with open(self.filename,"r") as f:
