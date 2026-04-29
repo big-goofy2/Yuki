94b40ea84f38a42e1760317c29363619087bd5b5
discpic-ron <granddriod1210@gmail.com>
2026-04-29 16:40:54 -0400

    Add memory management methods for storing data

--- source/memory.py
+++ source/memory.py
@@ -6,10 +6,25 @@ class Memory:
     self.short_term = []
     self.long_term = {}
     
+  def remember(self, key, value):
+    self.long_term[key] = value
+    
+  def recall(self, key):
+      return self.long_term.get(key)
+  
+  def add_short_term(self, message):
+      self.short_term.append(message)
+      return self.short_term
+    
   def save(self):
    with open(self.filename, "w") as f:
     json.dump({"short_term": self.short_term, "long_term": self.long_term}, f, indent=2)
    
   def load(self):
-   with open(self.filename,"r") as f:
-    return json.load(f)
+    try:
+     with open(self.filename,"r") as f:
+      data = json.load(f)
+      self.short_term = data.get("short_term", [])
+      self.long_term = data.get("long_term", {})
+    except FileNotFoundError:
+      return
